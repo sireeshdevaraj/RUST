1e44afa98bf909a5c1cdf72ee6fc76729671c4ad
『KūHaku』 <kongarasireeshdevaraj@gmail.com>
2022-04-19 15:24:45 +0530

    basic BS

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
+    let mut a=10;
+    a=50;
     println!("Hello, world!");
+    is_another();
+    let x=-2;
+    let y=x-1;
+    let bla="bla bla bla";
+    println!("the bla is {}",bla);
+    println!("the value of y is {} and x is {}",y,x);
+    println!("the value of a after getting mutated {}",a);
+    if y>x{
+        println!("yup the value of y is more");
+    }
+    else{
+        println!("LUL")
+    }
+    let n=1;
+    for n in 0..10{
+        let mut z=n+1;
+        println!("{z}");
+    }
 }
+fn is_another() {
+    let a=true;
+    println!("{} is ",a);
+}
+//time to move to module imports and DSA
